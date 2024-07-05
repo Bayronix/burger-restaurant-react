68d7a0aabf252284c9bfbc72871874ea43bdcd30
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-07-05 23:00:50 +0200

    .

---
+++ src/modules/Our-menu/Dishes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Our-menu.module.css";
+import PropTypes from "prop-types";
+
+const DishItem = ({ dish, openModal }) => {
+  return (
+    <li className={styles.dishItem}>
+      <img src={dish.image} alt={dish.name} className={styles.dishItemImage} />
+      <h3 className={styles.dishTitle}>{dish.name}</h3>
+      <p className={styles.dishPrice}>${dish.price}</p>
+      <button className={styles.button} onClick={() => openModal(dish)}>
+        Order
+      </button>
+    </li>
+  );
+};
+
+DishItem.propTypes = {
+  dish: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    components: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+  openModal: PropTypes.func.isRequired,
+};
+
+export default DishItem;
--- src/modules/Our-menu/Our-menu.jsx
+++ src/modules/Our-menu/Our-menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Modal from "react-modal";
 import styles from "./Our-menu.module.css";
+import DishItem from "./Dishes";
 
 const customStyles = {
   content: {
@@ -13,10 +14,92 @@ const customStyles = {
     maxWidth: "80%",
     maxHeight: "80%",
     overflow: "auto",
+    zIndex: 1,
   },
 };
 
 Modal.setAppElement("#root");
+
+const dishes = [
+  {
+    name: "Hamburger",
+    price: 3,
+    image: "/images/burger1.jpg",
+    components: [
+      "Bun",
+      "Beef Patty",
+      "Lettuce",
+      "Tomato",
+      "Onion",
+      "Pickles",
+      "Ketchup",
+      "Mustard",
+    ],
+  },
+  {
+    name: "Cheeseburger",
+    price: 5,
+    image: "/images/burger2.jpg",
+    components: [
+      "Bun",
+      "Beef Patty",
+      "Cheese",
+      "Lettuce",
+      "Tomato",
+      "Onion",
+      "Pickles",
+      "Ketchup",
+      "Mustard",
+    ],
+  },
+  {
+    name: "Bacon Burger",
+    price: 4,
+    image: "/images/burger3.jpg",
+    components: [
+      "Bun",
+      "Beef Patty",
+      "Bacon",
+      "Cheese",
+      "Lettuce",
+      "Tomato",
+      "Onion",
+      "Pickles",
+      "Ketchup",
+      "Mustard",
+    ],
+  },
+  {
+    name: "Vegetarian Burger",
+    price: 7,
+    image: "/images/burger4.jpg",
+    components: [
+      "Bun",
+      "Vegetarian Patty",
+      "Lettuce",
+      "Tomato",
+      "Onion",
+      "Pickles",
+      "Ketchup",
+      "Mustard",
+    ],
+  },
+  {
+    name: "Turkey Burger",
+    price: 9,
+    image: "/images/burger5.jpg",
+    components: [
+      "Bun",
+      "Turkey Patty",
+      "Lettuce",
+      "Tomato",
+      "Onion",
+      "Pickles",
+      "Ketchup",
+      "Mustard",
+    ],
+  },
+];
 
 const OurMenu = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -36,111 +119,9 @@ const OurMenu = () => {
     <section className={styles.ourDishesSection}>
       <h3 className={styles.ourMenu}>Our Menu</h3>
       <ul className={styles.dishesList}>
-        <li className={styles.dishItem}>
-          <img
-            src="/images/burger1.jpg"
-            alt="Mini Hamburger"
-            className={styles.dishItemImage}
-          />
-          <h3 className={styles.dishTitle}>Hamburger</h3>
-          <p className={styles.dishPrice}>$3</p>
-          <button
-            className={styles.button}
-            onClick={() =>
-              openModal({
-                name: "Hamburger",
-                price: 3,
-                image: "/images/burger1.jpg",
-              })
-            }
-          >
-            Order
-          </button>
-        </li>
-        <li className={styles.dishItem}>
-          <img
-            src="/images/burger2.jpg"
-            alt="Mini Hamburger"
-            className={styles.dishItemImage}
-          />
-          <h3 className={styles.dishTitle}>Cheeseburger</h3>
-          <p className={styles.dishPrice}>$5</p>
-          <button
-            className={styles.button}
-            onClick={() =>
-              openModal({
-                name: "Hamburger",
-                price: 3,
-                image: "/images/burger2.jpg",
-              })
-            }
-          >
-            Order
-          </button>
-        </li>
-        <li className={styles.dishItem}>
-          <img
-            src="/images/burger3.jpg"
-            alt="Mini Hamburger"
-            className={styles.dishItemImage}
-          />
-          <h3 className={styles.dishTitle}>Bacon Burger</h3>
-          <p className={styles.dishPrice}>$4</p>
-          <button
-            className={styles.button}
-            onClick={() =>
-              openModal({
-                name: "Hamburger",
-                price: 3,
-                image: "/images/burger3.jpg",
-              })
-            }
-          >
-            Order
-          </button>
-        </li>
-        <li className={styles.dishItem}>
-          <img
-            src="/images/burger4.jpg"
-            alt="Mini Hamburger"
-            className={styles.dishItemImage}
-          />
-          <h3 className={styles.dishTitle}>Vegetarian Burger</h3>
-          <p className={styles.dishPrice}>$7</p>
-          <button
-            className={styles.button}
-            onClick={() =>
-              openModal({
-                name: "Hamburger",
-                price: 3,
-                image: "/images/burger4.jpg",
-              })
-            }
-          >
-            Order
-          </button>
-        </li>
-        <li className={styles.dishItem}>
-          <img
-            src="/images/burger5.jpg"
-            alt="Mini Hamburger"
-            className={styles.dishItemImage}
-          />
-          <h3 className={styles.dishTitle}>Turkey Burger</h3>
-          <p className={styles.dishPrice}>$9</p>
-          <button
-            className={styles.button}
-            onClick={() =>
-              openModal({
-                name: "Hamburger",
-                price: 3,
-                image: "/images/burger5.jpg",
-              })
-            }
-          >
-            Order
-          </button>
-        </li>
+        {dishes.map((dish, index) => (
+          <DishItem key={index} dish={dish} openModal={openModal} />
+        ))}
       </ul>
 
       <Modal
@@ -152,14 +133,24 @@ const OurMenu = () => {
         {selectedDish && (
           <>
             <h2>{selectedDish.name}</h2>
+            <button className={styles.modalButton} onClick={closeModal}>
+              Close
+            </button>
             <img
               src={selectedDish.image}
               alt={selectedDish.name}
               className={styles.modalImage}
             />
+
             <p>Price: ${selectedDish.price}</p>
+            <h4>Components:</h4>
+            <ul>
+              {selectedDish.components.map((component, index) => (
+                <li key={index}>{component}</li>
+              ))}
+            </ul>
             <button className={styles.modalButton} onClick={closeModal}>
-              Close Modal
+              Order
             </button>
           </>
         )}
