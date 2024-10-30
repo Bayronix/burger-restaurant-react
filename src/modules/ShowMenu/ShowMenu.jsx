ad594beb0cfadd9cb458f8ed7eac3109ee4584ee
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-10-30 16:30:15 +0100

    comm

--- src/modules/ShowMenu/ShowMenu.jsx
+++ src/modules/ShowMenu/ShowMenu.jsx
@@ -3,7 +3,12 @@ import style from "./ShowMenu.module.css";
 
 const ShowMenu = () => {
   return (
+    <section className={style.showMenuSection}>
+      <div>
+      <h3 className={style.menuBorder}>Menu</h3>
+      </div>
     <div className={style.showMenuContainer} id="ShowMenu">
+      
       <ul className={style.showMenuList}>
         <li className={style.showMenuElement}>
           <NavLink to="/OurMenu#burgers">
@@ -12,17 +17,13 @@ const ShowMenu = () => {
           </NavLink>
         </li>
         <li className={style.showMenuElement}>
-          <NavLink to="/OurMenu#drinks">
-            <img src="../../../public/images/drinks/cocaCola.jpg" alt="Drinks" />
-            <p>Drinks:</p>
-          </NavLink>
-        </li>
-        <li className={style.showMenuElement}>
           <NavLink to="/OurMenu">
             <img src="../../../public/images/sides/sides.jpg" alt="Sides:" />
             <p>Sides:</p>
           </NavLink>
         </li>
+       
+        
         <li className={style.showMenuElement}>
           <NavLink to="/OurMenu">
             <img src="#" alt="Desserts:" />
@@ -36,8 +37,15 @@ const ShowMenu = () => {
             <p>Combo Meals:</p>
           </NavLink>
         </li>
+        <li className={style.showMenuElement}>
+          <NavLink to="/OurMenu#drinks">
+            <img src="../../../public/images/drinks/cocaCola.jpg" alt="Drinks" />
+            <p>Drinks:</p>
+          </NavLink>
+        </li>
       </ul>
     </div>
+    </section>
   );
 };
 
